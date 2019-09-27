e6801491e11a93f90755eb712e272cf6a11f3641
haythemmz <haythemwc@gmail.com>
2019-09-27 08:49:32 +0100

    fe nlp

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 #%%
 import pandas as pd 
 import pickle 
+import re 
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+import nltk 
+from nltk.corpus import stopwords
+
+porter = PorterStemmer()
+lancaster=LancasterStemmer()
 #%%
 filename="svm_bow.sav"
 loaded_model = pickle.load(open(filename, 'rb'))
@@ -15,9 +23,11 @@ def normalizer_all(tweet):
     tweets =' '.join([porter.stem(w) for w in tweets])
     return tweets
 
-
+message_love=" I loving man kiss"
 clean_message=normalizer_all(message_love)
+print(clean_message)
 
-a=vectorizer.transform([clean_message])
+
+#a=vectorizer.transform([clean_message])
 
 #%%
